fe2230765915f58ec02e7e99d5ab911b65e9b75d
vishnuchandrappan <cvishnu70@gmail.com>
2020-05-05 20:26:02 +0530

    express req params and query

--- 06-express-helloworld/app.js
+++ 06-express-helloworld/app.js
@@ -9,6 +9,28 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+/**
+ * Request Params
+ *
+ * http://localhost:3000/me/John/18
+ */
+app.get("/me/:name/:age", (req, res) => {
+  res.send(
+    `Hallo Welt, ich heiße ${req.params.name} und Ich bin ${req.params.age} Jahre alt`
+  );
+});
+
+/** Query String
+ *
+ * http://localhost:3000/search?q=king
+ */
+app.get("/search", (req, res) => {
+  res.send(`You searched for ${req.query.q}`);
+});
 
 /** Port */
 app.listen(3000);
